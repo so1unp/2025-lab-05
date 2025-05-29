867932ee3c3c601409b328655f8935a342853e1f
fabianvm <vidalfabian013@gmail.com>
2025-05-29 16:38:50 -0300

    a chambear

--- catacumbas/server.c
+++ catacumbas/server.c
@@ -2,6 +2,30 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+
+struct Tesoro {
+    int local_id;
+    int fila;
+    int columna;
+};
+
+struct Jugador {
+    int local_id;
+    int fila;
+    int columna;
+    char tipo;
+    int mov_fila;
+    int mov_columna;
+};
+
+struct Arena {
+    int max_participantes;
+    int mapa[1];
+    struct Tesoro tesoros[1];
+    struct Jugador jugadores[1];
+};
+
+
 int main(int argc, char* argv[])
 {
     printf("Catacumba\n");
